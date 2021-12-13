a7e1cee0d89c51aa14fbfb1f972b3bae978bc556
D99ZY <dtriegaardt@outlook.com>
2021-12-13 16:51:20 +0000

    Add components to App

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,9 +1,16 @@
+import Location from './components/Location';
+import Weather from './components/Weather';
+import Search from './components/Search';
 import './App.css';
 
 function App() {
     return (
         <div className="app">
-
+            <div className="app--container">
+                <Location city='Durban' country='ZA' />
+                <Weather temperature={27} conditions='Cloudy' />
+                <Search />
+            </div>
         </div>
     );
 }
