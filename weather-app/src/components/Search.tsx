5a07ae2442c3ac313499ffb86499a63e71ff4f6d
D99ZY <dtriegaardt@outlook.com>
2022-02-04 19:09:09 +0000

    Add fetch weather api functionality

--- weather-app/src/components/Search.tsx
+++ weather-app/src/components/Search.tsx
@@ -1,16 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import { Weather } from '../context/types';
+import { updateWeather } from '../context/actions';
 import './Search.css';
 
 const Search = () => {
+    // Context
+    const { dispatch } = useContext(GlobalContext);
+
     // State
     const [text, setText] = useState<string>('');
 
-    // Update weather in global state
-    const onSubmitHandler = (e: React.FormEvent) => {
-        e.preventDefault();
+    // Constants
+    const BASE_URL: string = 'https://api.openweathermap.org/data/2.5/weather';
+    const WEATHER_API_KEY = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
 
-        console.log('Text', text);
-    }
+    // Submit button function
+    const onSubmitHandler = (e: React.FormEvent) => {
+        e.preventDefault(); // Prevent page reload
+
+        // Check for empty text box
+
+
+        fetchWeather();
+    };
+    
+    // Update weather in global state
+    const fetchWeather = () => {
+        let newWeather: Weather;
+
+        fetch(BASE_URL + '?q=' + text + '&appid=' + WEATHER_API_KEY)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw res;
+                }
+            })
+            .then((data) => {
+                newWeather = {
+                    city: data.name,
+                    country: data.sys.country,
+                    temperature: data.main.temp,
+                    conditions: data.weather[0].main
+                };
+                dispatch(updateWeather(newWeather));
+            })
+            .catch((err) => {
+                console.log('Error:', err);
+            })
+    };
 
     return (
         <div className='search'>
