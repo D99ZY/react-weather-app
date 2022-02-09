db5e46f0cf774596260545f84b4fd21fea238731
D99ZY <dtriegaardt@outlook.com>
2022-02-09 17:22:40 +0000

    Add fetch time api functionality

--- weather-app/src/components/Location.tsx
+++ weather-app/src/components/Location.tsx
@@ -1,20 +1,56 @@
-import { useContext } from 'react';
+import { useCallback, useContext, useEffect } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import { Time } from '../context/types';
+import { updateTime } from '../context/actions';
 import './Location.css';
 
 const Location = () => {
 
     // Context
     const {
-        state: { weather }
-    } = useContext(GlobalContext)
+        dispatch,
+        state: { weather, time }
+    } = useContext(GlobalContext);
 
+    // Constants
+    const TIME_BASE_URL: string = 'https://dev.virtualearth.net/REST/v1/timezone/';
+    const TIME_API_KEY = process.env.REACT_APP_TIME_API_KEY;
+
+    // Update time in global state
+    const fetchTime = useCallback(async () => {
+
+        let newTime: Time;
+
+        const res = await fetch(TIME_BASE_URL + weather.lat + ',' + weather.long + '?key=' + TIME_API_KEY);
+        const data = await res.json();
+
+        let localTime: string = data.resourceSets[0].resources[0].timeZone.convertedTime.localTime;        
+        let localTimeArray: string[] = localTime.replaceAll('-', '$').replaceAll('T', '$').replaceAll(':', '$').split('$');
+
+        newTime = {
+            year: localTimeArray[0],
+            month: localTimeArray[1],
+            day: localTimeArray[2],
+            hour: localTimeArray[3],
+            minute: localTimeArray[4],
+        };
+
+        dispatch(updateTime(newTime));
+    }, [TIME_API_KEY, dispatch, weather.lat, weather.long]);
+
+    useEffect(() => {
+        fetchTime();
+    }, [fetchTime, weather]);
+
+    // JSX
     return (
         <div className='location'>
             <h2>
                 {weather.city}, {weather.country}
             </h2>
-            <h4>14:32 13/12/2021</h4>
+            <h4>
+                {time.hour}:{time.minute} {time.day}/{time.month}/{time.year}
+            </h4>
         </div>
     );
 };
