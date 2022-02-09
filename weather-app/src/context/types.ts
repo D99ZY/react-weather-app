92932d02f58b288a623e8b4a0048c32784c8fc7b
D99ZY <dtriegaardt@outlook.com>
2022-02-09 17:26:58 +0000

    Add type time

--- weather-app/src/context/types.ts
+++ weather-app/src/context/types.ts
@@ -4,18 +4,28 @@ import { Dispatch } from 'react';
 export type Weather = {
     city: string;
     country: string;
+    lat: number;
+    long: number;
     temperature: number;
     conditions: string;
 };
 
-export type GlobalState = {
-    weather: Weather;
+export type Time = {
+    year: string;
+    month: string;
+    day: string;
+    hour: string;
+    minute: string;
 };
 
-export type Action = {
-    type: 'UPDATE_WEATHER'; 
-    payload: Weather;
+export type GlobalState = {
+    weather: Weather;
+    time: Time;
 };
+
+export type Action =
+   | { type: 'UPDATE_WEATHER'; payload: Weather; }
+   | { type: 'UPDATE_TIME'; payload: Time };
 
 export type Context = {
     state: GlobalState;
