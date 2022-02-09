f3e01809e421bb6e66eeea5f3a8294141dac7884
D99ZY <dtriegaardt@outlook.com>
2022-02-09 17:26:13 +0000

    Change default unit from K to C

--- weather-app/src/components/Weather.tsx
+++ weather-app/src/components/Weather.tsx
@@ -7,7 +7,7 @@ const Weather = () => {
     // Context
     const {
         state: { weather }
-    } = useContext(GlobalContext)
+    } = useContext(GlobalContext);
 
     return (
         <div className='weather'>
@@ -15,7 +15,7 @@ const Weather = () => {
                 // Add button to convert temp from C to F
             }
             <h1>
-                {weather.temperature}&deg;C
+                {Math.round(((weather.temperature - 273.15) + Number.EPSILON) * 10) / 10}&deg;C
             </h1>
             <h3>
                 {weather.conditions}
