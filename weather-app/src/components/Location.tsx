52e1dba09048890223b14074823f22c8f34a9128
D99ZY <dtriegaardt@outlook.com>
2022-02-10 16:41:29 +0000

    Added comments

--- weather-app/src/components/Location.tsx
+++ weather-app/src/components/Location.tsx
@@ -6,27 +6,31 @@ import './Location.css';
 
 const Location = () => {
 
+    // Constants
+    const TIME_BASE_URL: string = 'https://dev.virtualearth.net/REST/v1/timezone/';
+    const TIME_API_KEY = process.env.REACT_APP_TIME_API_KEY;
+    
     // Context
     const {
         dispatch,
         state: { weather, time }
     } = useContext(GlobalContext);
 
-    // Constants
-    const TIME_BASE_URL: string = 'https://dev.virtualearth.net/REST/v1/timezone/';
-    const TIME_API_KEY = process.env.REACT_APP_TIME_API_KEY;
-
     // Update time in global state
     const fetchTime = useCallback(async () => {
 
+        // Initiate newTime variable of type Time
         let newTime: Time;
 
+        // Call fetch API to retrieve data
         const res = await fetch(TIME_BASE_URL + weather.lat + ',' + weather.long + '?key=' + TIME_API_KEY);
         const data = await res.json();
 
+        // Convert date format from string <YYYY-MM-DDTHH:MM:SS> to string array
         let localTime: string = data.resourceSets[0].resources[0].timeZone.convertedTime.localTime;        
         let localTimeArray: string[] = localTime.replaceAll('-', '$').replaceAll('T', '$').replaceAll(':', '$').split('$');
 
+        // Populate newTime variable with API data
         newTime = {
             year: localTimeArray[0],
             month: localTimeArray[1],
@@ -35,9 +39,11 @@ const Location = () => {
             minute: localTimeArray[4],
         };
 
+        // Update global state with newTime
         dispatch(updateTime(newTime));
     }, [TIME_API_KEY, dispatch, weather.lat, weather.long]);
 
+    // Call fetchTime on first render and each time global state weather updates
     useEffect(() => {
         fetchTime();
     }, [fetchTime, weather]);
